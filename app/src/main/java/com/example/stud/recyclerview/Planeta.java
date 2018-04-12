e4ee9e45d8764ab37a550364d1b3019c8ac25340
Patryk Kudła <w57012@student.wsiz.rzeszow.pl>
2018-04-12 18:31:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stud/recyclerview/Planeta.java
@@ -0,0 +1,17 @@
+package com.example.stud.recyclerview;
+
+/**
+ * Created by W57012 on 2018-04-12.
+ */
+
+public class Planeta {
+    String nazwa;
+    String rodzaj;
+    int ksiezyce;
+
+    public Planeta(String nazwa, String rodzaj, int ksiezyce) {
+        this.nazwa = nazwa;
+        this.rodzaj = rodzaj;
+        this.ksiezyce = ksiezyce;
+    }
+}
